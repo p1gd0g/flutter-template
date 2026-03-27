a32f969f872a3b38f64674a0659c87fa908d236b
p1gd0g <renhao.szu@gmail.com>
2026-03-28 00:26:40 +0800

    添加 forui 依赖并更新主应用程序的主题和本地化设置

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:forui/forui.dart';
 import 'package:logger/logger.dart';
 
 final logger = Logger();
@@ -7,11 +9,25 @@ final logger = Logger();
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
+  final theme = const <TargetPlatform>{
+    TargetPlatform.android,
+    TargetPlatform.iOS,
+    TargetPlatform.fuchsia,
+  }.contains(defaultTargetPlatform)
+      ? FThemes.neutral.light.touch
+      : FThemes.neutral.light.desktop;
+
   runApp(
-    const ProviderScope(
+    ProviderScope(
       child: MaterialApp(
-        // theme: AppTheme.light,
-        home: Scaffold(),
+        supportedLocales: FLocalizations.supportedLocales,
+        localizationsDelegates: const [...FLocalizations.localizationsDelegates],
+        theme: theme.toApproximateMaterialTheme(),
+        builder: (_, child) => FTheme(
+          data: theme,
+          child: FToaster(child: FTooltipGroup(child: child!)),
+        ),
+        home: const FScaffold(child: Placeholder()),
       ),
     ),
   );
